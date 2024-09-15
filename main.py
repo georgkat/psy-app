42c401262ded029aa16cd41aa3441e02302f3075
Georgiy Katrechko <georgkat@yandex.ru>
2024-09-15 18:26:18 +0300

    minor to doc_reg

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ import datetime
 import mariadb
 import uuid
 import string
-from fastapi import FastAPI, applications, Request
+from fastapi import FastAPI, applications, Request, HTTPException
+from pydantic import ValidationError
 import random
 from json_actions import parse_doctor_register
 from models.actions import ActionUserLogin
@@ -40,7 +41,7 @@ config = {
 
 app = FastAPI()
 
-origins = ['http://localhost:3000', 'https://localhost:3000', 'http://127.0.0.1:3000']
+origins = ['http://localhost:3000', 'https://localhost:3000', 'http://127.0.0.1:3000', 'http://0.0.0.0:3000']
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -157,153 +158,156 @@ def register(data:UserCreate):
 @app.post('/register_therapist')
 # TODO сделать генерацию пользователя
 def register_therapist(data: DocRegister):
-    mail = data.doc_email
-    password = ''.join([random.choice(string.ascii_letters) + random.choice(string.digits) for i in range(0, 4)])
+    try:
+        mail = data.doc_email
+        password = ''.join([random.choice(string.ascii_letters) + random.choice(string.digits) for i in range(0, 4)])
 
-    con = mariadb.connect(**config)
-    cur = con.cursor()
-    cur.execute(f"SELECT * FROM users WHERE email = '{mail}';")
-    f = cur.fetchall()
-    if f == []:
-        cur.execute(f"INSERT INTO users (email, password) VALUES ('{mail}', '{password}');")
+        con = mariadb.connect(**config)
+        cur = con.cursor()
+        cur.execute(f"SELECT * FROM users WHERE email = '{mail}';")
+        f = cur.fetchall()
+        if f == []:
+            cur.execute(f"INSERT INTO users (email, password) VALUES ('{mail}', '{password}');")
+            con.commit()
+            cur.close()
+            con.close()
+        else:
+            cur.close()
+            con.close()
+            return {'status': False,
+                    'error': 'user_already_registred'}
+
+        con = mariadb.connect(**config)
+        cur = con.cursor()
+        cur.execute(f"SELECT id FROM users WHERE email = '{mail}' AND password = '{password}';")
+        f = cur.fetchall()
         con.commit()
         cur.close()
         con.close()
-    else:
+
+        date = datetime.datetime.now()
+        token = uuid.uuid4()
+        user_id = f[0][0]
+        sql = f"INSERT INTO tokens (user_id, token) VALUES ('{user_id}', '{token}');"
+        con = mariadb.connect(**config)
+        cur = con.cursor()
+        cur.execute(sql)
+        con.commit()
         cur.close()
         con.close()
-        return {'status': False,
-                'error': 'user_already_registred'}
 
-    con = mariadb.connect(**config)
-    cur = con.cursor()
-    cur.execute(f"SELECT id FROM users WHERE email = '{mail}' AND password = '{password}';")
-    f = cur.fetchall()
-    con.commit()
-    cur.close()
-    con.close()
-
-    date = datetime.datetime.now()
-    token = uuid.uuid4()
-    user_id = f[0][0]
-    sql = f"INSERT INTO tokens (user_id, token) VALUES ('{user_id}', '{token}');"
-    con = mariadb.connect(**config)
-    cur = con.cursor()
-    cur.execute(sql)
-    con.commit()
-    cur.close()
-    con.close()
-
-    doc_id = user_id
-    columns = 'doc_id, doc_name, doc_date_of_birth, doc_gender, doc_edu, doc_method, doc_method_other, doc_language, doc_edu_additional, doc_comunity, doc_practice_start, doc_online_experience, doc_customers_amount_current, doc_therapy_length, doc_personal_therapy, doc_supervision, doc_another_job, doc_customers_slots_available, doc_socials_links, doc_citizenship, doc_citizenship_other, doc_ref, doc_ref_other, doc_phone, doc_email, doc_additional_info, doc_question_1, doc_question_2, doc_contact, user_photo'
-    # data.user_photo = [x.replace("'", '"') for x in data.user_photo]
-    # print(data.user_photo)
+        doc_id = user_id
+        columns = 'doc_id, doc_name, doc_date_of_birth, doc_gender, doc_edu, doc_method, doc_method_other, doc_language, doc_edu_additional, doc_comunity, doc_practice_start, doc_online_experience, doc_customers_amount_current, doc_therapy_length, doc_personal_therapy, doc_supervision, doc_another_job, doc_customers_slots_available, doc_socials_links, doc_citizenship, doc_citizenship_other, doc_ref, doc_ref_other, doc_phone, doc_email, doc_additional_info, doc_question_1, doc_question_2, doc_contact, user_photo'
+        # data.user_photo = [x.replace("'", '"') for x in data.user_photo]
+        # print(data.user_photo)
 
 
-    # save photos
-    photos = ', '.join([f"('{str(x)}')" for x in data.user_photo])
-    print(photos)
-    if photos:
-        sql = f'INSERT INTO images (img) VALUES {photos} RETURNING img_id;'
+        # save photos
+        photos = ', '.join([f"('{str(x)}')" for x in data.user_photo])
+        print(photos)
+        if photos:
+            sql = f'INSERT INTO images (img) VALUES {photos} RETURNING img_id;'
+            print(sql)
+            con = mariadb.connect(**config)
+            cur = con.cursor()
+            cur.execute(sql)
+            f = cur.fetchall()
+            print('photos added')
+            con.commit()
+            cur.close()
+            con.close()
+
+            photo_ids = ', '.join([str(x[0]) for x in f])
+            print(photo_ids)
+            data.user_photo = photo_ids
+        else:
+            data.user_photo = ''
+
+        items = ', '.join([f"'{str(x)}'" for x in data.model_dump().values()])
+
+        sql = f"INSERT INTO doctors ({columns}) VALUES ({doc_id}, {items})"
         print(sql)
+
+        con = mariadb.connect(**config)
+        cur = con.cursor()
+        cur.execute(sql)
+        con.commit()
+        cur.close()
+        con.close()
+
+        # take everything back with token
+        sql = f'SELECT doc_id, doc_name, doc_date_of_birth, doc_gender, doc_edu, doc_method, doc_method_other, doc_language, doc_edu_additional, doc_comunity, doc_practice_start, doc_online_experience, doc_customers_amount_current, doc_therapy_length, doc_personal_therapy, doc_supervision, doc_another_job, doc_customers_slots_available, doc_socials_links, doc_citizenship, doc_citizenship_other, doc_ref, doc_ref_other, doc_phone, doc_email, doc_additional_info, doc_question_1, doc_question_2, doc_contact, user_photo FROM doctors JOIN tokens ON doctors.doc_id = tokens.user_id WHERE token = "{token}"'
+
         con = mariadb.connect(**config)
         cur = con.cursor()
         cur.execute(sql)
         f = cur.fetchall()
-        print('photos added')
         con.commit()
         cur.close()
         con.close()
 
-        photo_ids = ', '.join([str(x[0]) for x in f])
-        print(photo_ids)
-        data.user_photo = photo_ids
-    else:
-        data.user_photo = ''
+        print('latest')
+        print(f)
+        doc_id, doc_photos_ids = f[0][0], f[0][29]
+        print(doc_id, doc_photos_ids)
 
-    items = ', '.join([f"'{str(x)}'" for x in data.model_dump().values()])
+        for i in f:
+            print(i)
 
-    sql = f"INSERT INTO doctors ({columns}) VALUES ({doc_id}, {items})"
-    print(sql)
+        if doc_photos_ids:
+            sql = f'SELECT img FROM images WHERE img_id IN ({doc_photos_ids})'
+            print(sql)
 
-    con = mariadb.connect(**config)
-    cur = con.cursor()
-    cur.execute(sql)
-    con.commit()
-    cur.close()
-    con.close()
+            con = mariadb.connect(**config)
+            cur = con.cursor()
+            cur.execute(sql)
+            fph = cur.fetchall()
+            con.commit()
+            cur.close()
+            con.close()
 
-    # take everything back with token
-    sql = f'SELECT doc_id, doc_name, doc_date_of_birth, doc_gender, doc_edu, doc_method, doc_method_other, doc_language, doc_edu_additional, doc_comunity, doc_practice_start, doc_online_experience, doc_customers_amount_current, doc_therapy_length, doc_personal_therapy, doc_supervision, doc_another_job, doc_customers_slots_available, doc_socials_links, doc_citizenship, doc_citizenship_other, doc_ref, doc_ref_other, doc_phone, doc_email, doc_additional_info, doc_question_1, doc_question_2, doc_contact, user_photo FROM doctors JOIN tokens ON doctors.doc_id = tokens.user_id WHERE token = "{token}"'
+            fph = [ph[0] for ph in fph]
+        else:
+            fph = []
 
-    con = mariadb.connect(**config)
-    cur = con.cursor()
-    cur.execute(sql)
-    f = cur.fetchall()
-    con.commit()
-    cur.close()
-    con.close()
+        out = {'status': True,
+               'password': f'{password}',
+               'token': f'{token}',
+               'doc_name': f[0][1],
+               'doc_date_of_birth': f[0][2],
+               'doc_gender': f[0][3],
+               'doc_edu': f[0][4],
+               'doc_method': f[0][5],
+               'doc_method_other': f[0][6],
+               'doc_language': f[0][7],
+               'doc_edu_additional': f[0][8],
+               'doc_comunity': f[0][9],
+               'doc_practice_start': f[0][10],
+               'doc_online_experience': f[0][11],
+               'doc_customers_amount_current': f[0][12],
+               'doc_therapy_length': f[0][13],
+               'doc_personal_therapy': f[0][14],
+               'doc_supervision': f[0][15],
+               'doc_another_job': f[0][16],
+               'doc_customers_slots_available': f[0][17],
+               'doc_socials_links': f[0][18],
+               'doc_citizenship': f[0][19],
+               'doc_citizenship_other': f[0][20],
+               'doc_ref': f[0][21],
+               'doc_ref_other': f[0][22],
+               'doc_phone': f[0][23],
+               'doc_email': f[0][24],
+               'doc_additional_info': f[0][25],
+               'doc_question_1': f[0][26],
+               'doc_question_2': f[0][27],
+               'doc_contact': f[0][28],
+               'user_photo': fph}
 
-    print('latest')
-    print(f)
-    doc_id, doc_photos_ids = f[0][0], f[0][29]
-    print(doc_id, doc_photos_ids)
+        print(out)
 
-    for i in f:
-        print(i)
-
-    if doc_photos_ids:
-        sql = f'SELECT img FROM images WHERE img_id IN ({doc_photos_ids})'
-        print(sql)
-
-        con = mariadb.connect(**config)
-        cur = con.cursor()
-        cur.execute(sql)
-        fph = cur.fetchall()
-        con.commit()
-        cur.close()
-        con.close()
-
-        fph = [ph[0] for ph in fph]
-    else:
-        fph = []
-
-    out = {'status': True,
-           'password': f'{password}',
-           'token': f'{token}',
-           'doc_name': f[0][1],
-           'doc_date_of_birth': f[0][2],
-           'doc_gender': f[0][3],
-           'doc_edu': f[0][4],
-           'doc_method': f[0][5],
-           'doc_method_other': f[0][6],
-           'doc_language': f[0][7],
-           'doc_edu_additional': f[0][8],
-           'doc_comunity': f[0][9],
-           'doc_practice_start': f[0][10],
-           'doc_online_experience': f[0][11],
-           'doc_customers_amount_current': f[0][12],
-           'doc_therapy_length': f[0][13],
-           'doc_personal_therapy': f[0][14],
-           'doc_supervision': f[0][15],
-           'doc_another_job': f[0][16],
-           'doc_customers_slots_available': f[0][17],
-           'doc_socials_links': f[0][18],
-           'doc_citizenship': f[0][19],
-           'doc_citizenship_other': f[0][20],
-           'doc_ref': f[0][21],
-           'doc_ref_other': f[0][22],
-           'doc_phone': f[0][23],
-           'doc_email': f[0][24],
-           'doc_additional_info': f[0][25],
-           'doc_question_1': f[0][26],
-           'doc_question_2': f[0][27],
-           'doc_contact': f[0][28],
-           'user_photo': fph}
-
-    print(out)
-
-    return out
+        return out
+    except ValidationError as e:
+        raise HTTPException(status_code=422, detail=str(e))
 
 
 
