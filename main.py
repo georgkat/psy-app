faca0bd554162e3bf29d9d593e6d17321c98df5a
Georgiy Katrechko <georgkat@yandex.ru>
2024-11-18 16:54:30 +0300

    client_photos

--- main.py
+++ main.py
@@ -365,7 +365,7 @@ def return_client_data(data: SingleToken):
         if pre_out:
             for i in range(0, 14):
                 out[desc[i][0]] = fetch_0[0][i]
-                if fetch_0[0][16]:
+                if fetch_0[0][14]:
                     out["user_photo"] = str(fetch_0[0][16]) + ';' + str(fetch_0[0][14].decode())
                 else:
                     out['user_photo'] = ""
